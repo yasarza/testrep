906a4842a08b9137bbd64cb8f72209af3265550a
yasarza <43999349+yasarza@users.noreply.github.com>
2020-11-03 11:06:34 -0800

    Update wellcome.py

--- wellcome.py
+++ wellcome.py
@@ -1,10 +1,16 @@
-import math
-
-x = 3
-
-print(pow(x,3))
-
-
-multiply = lambda x,y : x * y
-
-print(multiply(5,6))
+class MyClass(object):
+    instance_count = 0
+    def __init__(self, value):
+        self.__value = value
+        MyClass.instance_count += 1
+        print("instance No {} created".format(MyClass.instance_count))
+    def aMethod(self, aValue):
+        self.__value *= aValue
+    def __str__(self):
+        return "A MyClass instance with value: " + str(self.__value)
+    def __del__(self):
+        MyClass.instance_count -= 1
+        
+num = MyClass(42)
+num.aMethod(66)
+print(num)
